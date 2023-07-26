42f7b4be4a7fd480fd02462cb092393e26baf466
sunkeydokey <sunkeydokey@gmail.com>
2023-07-26 19:19:27 +0900

    다이나믹 라우팅까지 완료

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,7 +1,48 @@
+import { getAllPostIds, getPostData } from '@/lib/posts';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
 import React from 'react';
 
-const Post = () => {
-  return <div></div>;
+const Post = ({
+  postData,
+}: {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+}) => {
+  const { title, date, contentHtml } = postData;
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <article>
+        <h1>{title}</h1>
+        <div>{date}</div>
+        <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
+      </article>
+    </div>
+  );
 };
 
 export default Post;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds();
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params?.id as string);
+  return {
+    props: {
+      postData,
+    },
+  };
+};
